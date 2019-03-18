a1707b8f9e705cdaf14b27d7dd380f6a224c375f
sanleny <308644773@qq.com>
2019-03-18 17:27:43 +0800

    代码优化

--- study-v1/client-v1/src/main/java/cn/sanleny/study/springcloud/client/ClientApplication.java
+++ study-v1/client-v1/src/main/java/cn/sanleny/study/springcloud/client/ClientApplication.java
@@ -12,9 +12,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class Application {
+public class ClientApplication {
 
     public static void main(String args[]){
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(ClientApplication.class,args);
     }
 }
