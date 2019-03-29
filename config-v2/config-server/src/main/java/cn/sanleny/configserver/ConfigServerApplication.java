926160afa31ed02d9c4a92cfc20cbadb80426ce3
sanleny <308644773@qq.com>
2019-03-29 17:26:54 +0800

    springCloud 配置中心和注册中心结合使用

--- config-v2/config-server/src/main/java/cn/sanleny/configserver/ConfigServerApplication.java
+++ config-v2/config-server/src/main/java/cn/sanleny/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package cn.sanleny.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient//配置中心和注册中心结合使用  引入jar,将自己定义为一个客户端
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
