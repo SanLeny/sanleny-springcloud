8db3e6e0aba201cda3062f331fbebb315551df88
sanleny <308644773@qq.com>
2019-04-08 17:16:33 +0800

    链路追踪 zipkin

--- ribbon-v3/client-v3/src/main/java/cn/sanleny/study/springcloud/client/Client3Application.java
+++ ribbon-v3/client-v3/src/main/java/cn/sanleny/study/springcloud/client/Client3Application.java
@@ -1,9 +1,11 @@
 package cn.sanleny.study.springcloud.client;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Author: sanleny
@@ -18,5 +20,10 @@ public class Client3Application {
 
     public static void main(String args[]){
         SpringApplication.run(Client3Application.class,args);
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
