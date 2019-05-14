fac682bc55e2de0debd47282f442ab5d350e290e
sanleny <308644773@qq.com>
2019-05-14 16:04:07 +0800

    demo update

--- ribbon-v3/client-v3/src/main/java/cn/sanleny/study/springcloud/client/Client3Application.java
+++ ribbon-v3/client-v3/src/main/java/cn/sanleny/study/springcloud/client/Client3Application.java
@@ -1,11 +1,8 @@
 package cn.sanleny.study.springcloud.client;
 
-import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @Author: sanleny
@@ -22,8 +19,8 @@ public class Client3Application {
         SpringApplication.run(Client3Application.class,args);
     }
 
-    @Bean
-    public Sampler defaultSampler() {
-        return Sampler.ALWAYS_SAMPLE;
-    }
+//    @Bean
+//    public Sampler defaultSampler() {
+//        return Sampler.ALWAYS_SAMPLE;
+//    }
 }
