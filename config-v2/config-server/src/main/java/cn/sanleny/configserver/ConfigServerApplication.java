8ba2d3d934ab1b8c90f00f2c85085fc0765aabcc
sanleny <308644773@qq.com>
2019-05-14 10:19:08 +0800

    config-v2 update  add 自定义接口 配合webhook刷新示例

--- config-v2/config-server/src/main/java/cn/sanleny/configserver/ConfigServerApplication.java
+++ config-v2/config-server/src/main/java/cn/sanleny/configserver/ConfigServerApplication.java
@@ -1,9 +1,13 @@
 package cn.sanleny.configserver;
 
+import cn.hutool.http.Header;
+import cn.hutool.http.HttpRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * http://localhost:5000/configClient/dev  查看配置
@@ -11,10 +15,23 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient//配置中心和注册中心结合使用  引入jar,将自己定义为一个客户端
+@RestController
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+
+    /**
+     * 自定义刷新
+     * @throws Exception
+     */
+    @RequestMapping("/refresh")
+    public void refresh() throws Exception {
+        String url="http://localhost:5000/actuator/bus-refresh";
+        String body = HttpRequest.post(url).header(Header.CONTENT_TYPE, "application/json;charset=UTF-8").execute().body();
+        System.out.println("==========refresh===="+body);
     }
 
 }
