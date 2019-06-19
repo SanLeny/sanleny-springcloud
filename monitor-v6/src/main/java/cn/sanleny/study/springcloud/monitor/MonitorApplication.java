ebf9eac53df6077d40e2d0edff54abe2cc6357bf
sanleny <308644773@qq.com>
2019-06-19 16:55:01 +0800

    update:monitor add spring-boot-admin

--- monitor-v6/src/main/java/cn/sanleny/study/springcloud/monitor/MonitorApplication.java
+++ monitor-v6/src/main/java/cn/sanleny/study/springcloud/monitor/MonitorApplication.java
@@ -1,5 +1,6 @@
 package cn.sanleny.study.springcloud.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -17,6 +18,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableHystrixDashboard
 @EnableTurbine
 @SpringBootApplication
+@EnableAdminServer
 public class MonitorApplication {
 
     public static void main(String[] args) {
